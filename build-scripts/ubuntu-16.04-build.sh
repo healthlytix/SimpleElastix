072ab0d99ad0ee924a1028179ccc17d680c8d3d9
Brian Keating <bkeating@healthlytix.com>
2020-09-01 16:12:34 -0700

    Add pypi upload for linux build; temporarily disable GH actions

--- build-scripts/ubuntu-16.04-build.sh
+++ build-scripts/ubuntu-16.04-build.sh
@@ -4,8 +4,8 @@ sudo apt install cmake
 cd $GITHUB_WORKSPACE
 mkdir build
 cd build
-cmake -DBUILD_EXAMPLES:BOOL=OFF -DBUILD_TESTING:BOOL=OFF -DWRAP_TCL:BOOL=OFF -DCMAKE_BUILD_TYPE=Release ../SuperBuild
-make -j4
+cmake ../SuperBuild
+make -j2
 cd SimpleITK-build/Wrapping/Python
 sudo python Packaging/setup.py install
 
@@ -14,4 +14,11 @@ python Packaging/setup.py bdist_wheel -d wheel-dist
 ls -lh wheel-dist  # show the generated wheel file
 
 # fake a manylinux build by simply renaming the wheel file
-mv wheel-dist/SimpleITK_Elastix-2.0.0rc2-cp38-cp38-linux_x86_64.whl wheel-dist/SimpleITK_Elastix-2.0.0rc2-cp38-cp38-manylinux1_x86_64.whl
+cd wheel-dist
+SRC_WHEEL_FILENAME=`ls`
+DEST_WHEEL_FILENAME=`echo $WHEEL_FILENAME | sed 's/linux/manylinux1/'`
+mv $SRC_WHEEL_FILENAME $DEST_WHEEL_FILENAME
+
+# upload to pypi
+pip install twine
+twine upload --repository-url https://test.pypi.org/legacy/ --username cortechslabs --password $PYPI_PASSWORD *
