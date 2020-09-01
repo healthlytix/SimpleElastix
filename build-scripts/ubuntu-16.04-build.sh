434303fffdd1b6f15a7662e0616c5336a9a46258
Brian Keating <bkeating@healthlytix.com>
2020-09-01 12:19:36 -0700

    rename linux wheel file; upload build wheel to github

--- build-scripts/ubuntu-16.04-build.sh
+++ build-scripts/ubuntu-16.04-build.sh
@@ -8,3 +8,10 @@ cmake -DBUILD_EXAMPLES:BOOL=OFF -DBUILD_TESTING:BOOL=OFF -DWRAP_TCL:BOOL=OFF -DC
 make -j2
 cd SimpleITK-build/Wrapping/Python
 sudo python Packaging/setup.py install
+
+# build wheel
+python Packaging/setup.py bdist_wheel -d wheel-dist
+ls -lh wheel-dist  # show the generated wheel file
+
+# fake a manylinux build by simply renaming the wheel file
+mv wheel-dist/SimpleITK_Elastix-2.0.0rc2-cp38-cp38-linux_x86_64.whl wheel-dist/SimpleITK_Elastix-2.0.0rc2-cp38-cp38-manylinux1_x86_64.whl
