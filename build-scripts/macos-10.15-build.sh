73dc37bcb0b906d080c3af1c8558702de33ac2e8
Brian Keating <bkeating@healthlytix.com>
2020-09-01 17:31:50 -0700

    Check python version on mac vm

--- build-scripts/macos-10.15-build.sh
+++ build-scripts/macos-10.15-build.sh
@@ -6,7 +6,11 @@ rbenv install 2.6.6  # need to reinstall to add headers
 cd $GITHUB_WORKSPACE
 mkdir build
 cd build
-cmake -DBUILD_EXAMPLES:BOOL=OFF -DBUILD_TESTING:BOOL=OFF -DWRAP_TCL:BOOL=OFF -DCMAKE_BUILD_TYPE=Release ../SuperBuild
-make -j4
+cmake -DBUILD_EXAMPLES:BOOL=OFF -DBUILD_TESTING:BOOL=OFF -DCMAKE_BUILD_TYPE=Release ../SuperBuild
+make -j2
 cd SimpleITK-build/Wrapping/Python
 sudo python Packaging/setup.py install
+
+# build wheel
+python Packaging/setup.py bdist_wheel -d wheel-dist
+ls -lh wheel-dist  # show the generated wheel file
